51b0acc875fa07e1bca1f11e46b293e8e610c92b
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2010-09-05 14:15:30 -0300

    #new adicionado analytics

--- personal_website.rb
+++ personal_website.rb
@@ -19,6 +19,16 @@ __END__
   %head
     %meta(charset='utf-8')
     %title Sobre | Fabrício
+    :javascript
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-5295321-5']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
   %body
     %link{:rel => "stylesheet", :href => "/stylesheet.css", :type => "text/css"}
     = yield
