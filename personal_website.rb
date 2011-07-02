1118bc64eba69da26a1f38bef82612b870de328b
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-07-01 22:07:51 -0300

    atualizado layout e conteúdo

--- personal_website.rb
+++ personal_website.rb
@@ -36,97 +36,61 @@ __END__
 
 @@ index
 #primaryContent
-  #hello Olá.
-  %br
-  %strong Sou 
-  Fabrício Ferrari de Campos
-  %br
-  %strong Moro
-  Santo André - SP - Brasil
-  %br
-  %strong Trabalho
-  %a{:href => "http://www.vizir.com.br", :target=>"blank", :title =>  "Trabalho"}
-    Vizir
-  %br
-  %strong Gosto
-  %ul{:class => "noBullet"}
-    %li -- Desenvolver software
-    %ul{:class => "noBullet"}
-      %li --- Rails
-      %li --- Criar
-      %li --- Solucionar
-  %ul{:class => "noBullet"}
-    %li -- Testar software
-    %ul{:class => "noBullet"}
-      %li --- Investigação
-      %li --- Exploração
-      %li --- Imaginar
-  %ul{:class => "noBullet"}
-    %li -- Ajudar as pessoas
-    %ul{:class => "noBullet"}
-      %li --- Blogar
-      %li --- Comunidade
-  %ul{:class => "noBullet"}
-    %li -- Escutar
-    %ul{:class => "noBullet"}
-      %li --- Pessoas
-      %li --- Histórias
-      %li --- Rock'n' Roll
-  %ul{:class => "noBullet"}
-    %li -- Ler
-    %ul{:class => "noBullet"}
-      %li --- Aprender
-      %li --- Refletir
-      %li --- Viajar    
-  %ul{:class => "noBullet"}
-    %li -- Conversar
-    %ul{:class => "noBullet"}
-      %li --- Opinião
-      %li --- Sincero
-  %ul{:class => "noBullet"}
-    %li -- etc ;)
+  .box
+    #hello Olá.
+    %br
+    %strong Sou 
+    Fabrício Ferrari de Campos
+    %br
+    %strong Moro
+    Santo André - SP - Brasil
+    %br
+    %strong Trabalho
+    %a{:href => "http://f1studio.com.br", :target=>"blank", :title =>  "Trabalho"}
+      F1 Studio
 #secondaryContent
-  %strong +
-  #links
-    %br
-    %a{:href => "mailto:ffc.fabricio@gmail.com", :target=>"blank", :title =>  "E-mail"}
-      Contate-me
-    %br
-    %a{:href => "http://br.linkedin.com/in/fabricioferraridecampos", :target=>"blank", :title =>  "LinkedIn"}
-      LinkedIn - Minhas experiências e perfil profissional
-    %br
-    %a{:href => "http://github.com/FabricioFFC", :target=>"blank", :title =>  "Github"}
-      Projetos no github
-    %br
-    %a{:href => "http://twitter.com/FabricioFFC", :target=>"blank", :title =>  "Twitter"}
-      Twitter
-    %br
-    %a{:href => "http://qualidadebr.wordpress.com", :target=>"blank", :title =>  "QualidadeBR"}
-      QualidadeBR - meu blog focado em Teste e Qualidade de Software
-    %br
-    %a{:href => "http://www.testexpert.com.br/?q=blog/1886", :target=>"blank", :title =>  "TestExpert"}
-      Coluna no TestExpert
-    %br
-    %a{:href => "http://www.slideshare.net/FabricioFFC", :target=>"blank", :title =>  "Slideshare"}
-      Minhas apresentações no Slideshare
-    %br
-    %a{:href => "http://delicious.com/FabricioFFC", :target=>"blank", :title =>  "Delicious"}
-      Bookmarks no Delicious
-    %br
-    %a{:href => "http://www.facebook.com/ffc.fabricio", :target=>"blank", :title =>  "Facebook"}
-      Página do Facebook
-    %br
-    %a{:href => "http://www.google.com/profiles/ffc.fabricio", :target=>"blank", :title =>  "Google"}
-      Perfil no Google
-    %br
-    %a{:href => "http://blog.fabricioferrari.com.br/", :target=>"blank", :title =>  "Tumblr"}
-      Blog pessoal
-    %br
-    %a{:href => "http://www.lastfm.com.br/user/FabricioFFC", :target=>"blank", :title =>  "LastFM"}
-      O que gosto de escutar
-    %br
-    %a{:href => "http://www.google.com/reader/shared/ffc.fabricio", :target=>"blank", :title =>  "Google Reader"}
-      O que li na internet
-    %br
-    %a{:href => "http://moovee.me/user/FabricioFFC", :target=>"blank", :title =>  "Moovee"}
-      Filmes que assisti
+  .box
+    %strong +
+    #links
+      %br
+      %a{:href => "mailto:ffc.fabricio@gmail.com", :target=>"blank", :title =>  "E-mail"}
+        Contate-me
+      %br
+      %a{:href => "http://br.linkedin.com/in/fabricioferraridecampos", :target=>"blank", :title =>  "LinkedIn"}
+        LinkedIn - Minhas experiências e perfil profissional
+      %br
+      %a{:href => "http://github.com/FabricioFFC", :target=>"blank", :title =>  "Github"}
+        Projetos no github
+      %br
+      %a{:href => "http://twitter.com/FabricioFFC", :target=>"blank", :title =>  "Twitter"}
+        Twitter
+      %br
+      %a{:href => "http://qualidadebr.wordpress.com", :target=>"blank", :title =>  "QualidadeBR"}
+        QualidadeBR - meu blog focado em Teste e Qualidade de Software
+      %br
+      %a{:href => "http://www.testexpert.com.br/?q=blog/1886", :target=>"blank", :title =>  "TestExpert"}
+        Coluna no TestExpert
+      %br
+      %a{:href => "http://www.slideshare.net/FabricioFFC", :target=>"blank", :title =>  "Slideshare"}
+        Minhas apresentações no Slideshare
+      %br
+      %a{:href => "http://delicious.com/FabricioFFC", :target=>"blank", :title =>  "Delicious"}
+        Bookmarks no Delicious
+      %br
+      %a{:href => "http://www.facebook.com/ffc.fabricio", :target=>"blank", :title =>  "Facebook"}
+        Página do Facebook
+      %br
+      %a{:href => "http://www.google.com/profiles/ffc.fabricio", :target=>"blank", :title =>  "Google"}
+        Perfil no Google
+      %br
+      %a{:href => "http://blog.fabricioferrari.com.br/", :target=>"blank", :title =>  "Tumblr"}
+        Blog pessoal
+      %br
+      %a{:href => "http://www.lastfm.com.br/user/FabricioFFC", :target=>"blank", :title =>  "LastFM"}
+        O que gosto de escutar
+      %br
+      %a{:href => "http://www.google.com/reader/shared/ffc.fabricio", :target=>"blank", :title =>  "Google Reader"}
+        O que li na internet
+      %br
+      %a{:href => "http://moovee.me/user/FabricioFFC", :target=>"blank", :title =>  "Moovee"}
+        Filmes que assisti
